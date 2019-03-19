f695d1fe36334629df2b3cb945972423768dfe67
wllcstr <48567254+wllcstr@users.noreply.github.com>
2019-03-19 17:28:24 -0300

    Ancestors

--- tga/src/tga/BinarySearchTree.java
+++ tga/src/tga/BinarySearchTree.java
@@ -1,4 +1,5 @@
 package tga;
+import java.util.*;
 
 public class BinarySearchTree<K extends Comparable<K>,  V> implements BinarySearchTreeADT<K, V>  {
 
@@ -15,14 +16,30 @@ public class BinarySearchTree<K extends Comparable<K>,  V> implements BinarySear
         }
 
         public Node next(K other) {
-            return other.compareTo(key) < 0 ? left : right;
+            return other.compareTo(key) < 0 ? getLeft() : getRight();
         }
 
         public boolean isLeaf() {
-            return left == null && right == null;
+            return getLeft() == null && getRight() == null;
         }
         @Override
         public String toString() { return "" + key; }
+
+		public Node getLeft() {
+			return left;
+		}
+
+		public void setLeft(Node left) {
+			this.left = left;
+		}
+
+		public Node getRight() {
+			return right;
+		}
+
+		public void setRight(Node right) {
+			this.right = right;
+		}
     }
 
 	
@@ -60,9 +77,9 @@ public class BinarySearchTree<K extends Comparable<K>,  V> implements BinarySear
 	    if (node == null) {
 	        return new Node(key, value);
 	    } else if (key.compareTo(node.key) > 0) {
-	        node.right = insert(node.right, key, value);
+	        node.setRight(insert(node.getRight(), key, value));
 	    } else if (key.compareTo(node.key) < 0) {
-	        node.left = insert(node.left, key, value);
+	        node.setLeft(insert(node.getLeft(), key, value));
 	    }
 
 	    return node;
@@ -80,18 +97,18 @@ public class BinarySearchTree<K extends Comparable<K>,  V> implements BinarySear
 	    
 	    if (current == null) 
 	        return false;
-	    else if (current.left != null && current.right != null) {
+	    else if (current.getLeft() != null && current.getRight() != null) {
 	        // Caso 3
-	        Node tmp = current.left;     
-	        while (tmp.right != null) tmp = tmp.right;
+	        Node tmp = current.getLeft();     
+	        while (tmp.getRight() != null) tmp = tmp.getRight();
 	        deleteByCopying(tmp.key); 
 	        current.key = tmp.key; 
 	    } else {
 	        // Caso 1 ou Caso 2
-	        Node nextNode = current.right == null ? current.left : current.right;
+	        Node nextNode = current.getRight() == null ? current.getLeft() : current.getRight();
 	        if (current.equals(root)) root = nextNode;
-	        else if (parent.left.equals(current)) parent.left = nextNode;
-	        else parent.right = nextNode;
+	        else if (parent.getLeft().equals(current)) parent.setLeft(nextNode);
+	        else parent.setRight(nextNode);
 	    }
 
 	    return true;
@@ -106,8 +123,8 @@ public class BinarySearchTree<K extends Comparable<K>,  V> implements BinarySear
 		private void preOrder(Node node) {
 		   if (node != null) {
 		       System.out.print(node + " ");
-		       preOrder(node.left);
-		       preOrder(node.right);
+		       preOrder(node.getLeft());
+		       preOrder(node.getRight());
 		   }
 		}
 
@@ -119,9 +136,9 @@ public class BinarySearchTree<K extends Comparable<K>,  V> implements BinarySear
 
 	private void inOrder(Node node) {
 	   if (node != null) {
-	      inOrder(node.left);
+	      inOrder(node.getLeft());
 	      System.out.print(node + " ");
-	      inOrder(node.right);
+	      inOrder(node.getRight());
 	   }
 	}
 
@@ -133,8 +150,8 @@ public class BinarySearchTree<K extends Comparable<K>,  V> implements BinarySear
 
 	private void postOrder(Node node) {
 	   if (node != null) {
-	     postOrder(node.left);
-	     postOrder(node.right);
+	     postOrder(node.getLeft());
+	     postOrder(node.getRight());
 	     System.out.print(node + " ");
 	   }
 	}
@@ -145,30 +162,73 @@ public class BinarySearchTree<K extends Comparable<K>,  V> implements BinarySear
 		
 	}
 	
+    // Método semelhante ao search, porém retorna o nó
+    public BinarySearchTree<K, V>.Node nodeSearch(K key) {
+        return nodeSearch(root, key);
+    }
+    private BinarySearchTree<K, V>.Node nodeSearch(Node node, K key) {
+    	if (node == null) {
+	        return null;
+	    } else if (key.compareTo(node.key) == 0) {
+	        return node;
+	    }
+	    return nodeSearch(node.next(key), key);
+    }
+	
+	
+	public String ListAncestors(K key) {
+		return ListAncestors(root, key, "");
+	}
+	
+	private String ListAncestors(Node node, K key, String concat) {
+		// condições de saída
+		// caso não exista o nó (não encontrada a chave)
+		if(node == null)
+			return null;
+		// se a chave passada é da raiz, não tem ancestrais
+		if(root.key == key)
+			return "Esta chave não possui ancestrais";
+		// concatena o valor do ancestral
+		concat = concat + " " + node.toString();
+		// verifica a direção
+		int dir = node.key.compareTo(key);
+		if(dir < 0)
+			//verifica se não é o final
+			if(key.compareTo(node.getRight().key) == 0)
+				return concat;
+		if(dir > 0)
+			//verifica se não é o final
+			if(key.compareTo(node.getLeft().key) == 0)
+				return concat;
+			
+		return ListAncestors(node.next(key), key, concat);
+	}
+	
+	
 	@Override
 	public String toString() {
 	    return root == null ? "[empty]" : printTree(new StringBuffer());
 	}
 
 	private String printTree(StringBuffer sb) {
-	    if (root.right != null) {
-	        printTree(root.right, true, sb, "");
+	    if (root.getRight() != null) {
+	        printTree(root.getRight(), true, sb, "");
 	    }
 	    sb.append(root + "\n"); 
-	    if (root.left != null) {
-	        printTree(root.left, false, sb, "");
+	    if (root.getLeft() != null) {
+	        printTree(root.getLeft(), false, sb, "");
 	    }
 	        
 	    return sb.toString();
 	}
 
 	private void printTree(Node node, boolean isRight, StringBuffer sb, String indent) {
-	    if (node.right != null) {
-	        printTree(node.right, true, sb, indent + (isRight ? "        " : " |      "));
+	    if (node.getRight() != null) {
+	        printTree(node.getRight(), true, sb, indent + (isRight ? "        " : " |      "));
 	    }
 	    sb.append(indent + (isRight ? " /" : " \\") + "----- " + node + "\n"); 
-	    if (node.left != null) {
-	        printTree(node.left, false, sb, indent + (isRight ? " |      " : "        "));
+	    if (node.getLeft() != null) {
+	        printTree(node.getLeft(), false, sb, indent + (isRight ? " |      " : "        "));
 	    }
 	}
 
